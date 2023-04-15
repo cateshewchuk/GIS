e34a27455bab7269bc301b349daab77416c9e1be
cateshewchuk <cateshewchuk@gmail.com>
2023-04-15 15:07:37 -0400

    Update Interpolation.java

--- gui/src/main/java/gis/Algorithm/Interpolation.java
+++ gui/src/main/java/gis/Algorithm/Interpolation.java
@@ -5,7 +5,7 @@ import gis.triangulation.NotEnoughPointsException;
 import java.util.*;
 
 public class Interpolation {
-    public void runInterpolation (HashMap<String, Location> knownData, HashMap<String, Location> unknownData) throws NotEnoughPointsException {
+    public static void runInterpolation (HashMap<String, Location> knownData, HashMap<String, Location> unknownData) throws NotEnoughPointsException {
         SpatioInterpolation.queryValues(knownData, unknownData);
         TimeInterpolation.timeInterpolate(unknownData);
     }
